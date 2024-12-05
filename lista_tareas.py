7acfa3bb2d0e520403ca3da337ae5dc6c69495e4
sebastianVP <alex.valdezp22@gmail.com>
2024-12-04 21:53:49 -0500

    update de eliminar tareas

--- lista_tareas.py
+++ lista_tareas.py
@@ -9,6 +9,32 @@ def listar_tareas():
         for i, tarea in enumerate(tareas,1):
             print(f"{i}. {tarea}")
 
+def eliminar_tarea():
+    listar_tareas()
+    try:
+        index= int(input("Selecciona el numero de la tarea a eliminar"))-1
+        if 0<=index<len(tareas):
+            tarea_eliminada= tareas.pop(index)
+            print(f"Tarea '{tarea_eliminada}' eliminada")
+        else:
+            print("Numero de tarea no valido")
+    except ValueError:
+        print("Entrada no valida")
+
 if __name__ == "__main__":
     print("Bienvenidos a la aplicacion de lista de tareas")
-    listar_tareas()
+    while True:
+        print("\n1. Listar tareas")
+        print("2. eliminar tarea")
+        print("3. Salir")
+        opcion = input("Selecciona una opción: ")
+
+        if opcion == "1":
+            listar_tareas()
+        elif opcion == "2":
+            eliminar_tarea()
+        elif opcion == "3":
+            print("¡Hasta luego!")
+            break
+        else:
+            print("Opción no válida.")
