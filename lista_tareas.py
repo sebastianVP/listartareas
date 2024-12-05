3df9ba652e9dea7b188304cafc4943d63998fadf
sebastianVP <alex.valdezp22@gmail.com>
2024-12-04 21:44:42 -0500

    metodo nuevo agregar tarea

--- lista_tareas.py
+++ lista_tareas.py
@@ -9,6 +9,25 @@ def listar_tareas():
         for i, tarea in enumerate(tareas,1):
             print(f"{i}. {tarea}")
 
+def agregar_tarea():
+    tarea = input("Escribe una nueva tarea:")
+    tareas.append(tarea)
+    print(f"Tarea '{tarea}' añadidad exitosamente ")
+
+
 if __name__ == "__main__":
     print("Bienvenidos a la aplicacion de lista de tareas")
-    listar_tareas()
+    while True:
+        print("\n1. Listar tareas")
+        print("2. Agregar tarea")
+        print("3. Salir")
+        opcion = input("Selecciona una opcion:  ")
+        if opcion=="1":
+            listar_tareas()
+        elif opcion=="2":
+            agregar_tarea()
+        elif opcion=="3":
+            print("Salud, hasta luego!")
+            break
+        else:
+            print("Opción no valida")
